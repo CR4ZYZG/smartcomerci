9b4d43d6725aa730e505c934845a636ea886d862
CR4ZYZG <gabrielc.ftn1@gmail.com>
2021-11-24 11:27:11 -0300

    testes

--- cypress/pages/cadastro.page.js
+++ cypress/pages/cadastro.page.js
@@ -1,11 +1,19 @@
 import faker from 'faker'
 import Base from './_base.page';
+import { fakerBr } from 'js-brasil';
+
 
 const nome = faker.name.firstName();
+const dn = faker.date.between(1/1/1900, 1/1/2010);
+
 
 export default class Cad extends Base{
     static criarCadastro() {
-        cy.writeFile('cypress/fixtures/credenciais.json', { name: `${nome}`, email: 'eliza@example.com' })
-        cy.readFile('cypress/fixtures/credenciais.json')
+        cy.writeFile('cypress/fixtures/credenciais.json', { 
+            'name': `${faker.name.firstName()}`, 
+            'dataNascimento': `${faker.date.between(1900/1/1, 2010/1/1)}`,
+            'CPF': `${fakerBr.cpf()}`, 
+            'TF': `${faker.phone.phoneNumber()}` });
+     
     }   
 }
